f935926f64bd8a0cadfa94bb431766dc60b3079a
MonsitPasida <53083364+MonsitPasida@users.noreply.github.com>
2020-03-15 11:32:17 +0700

    first commit

---
+++ src/lab2/Box1.java
@@ -0,0 +1,14 @@
+
+package lab2;
+
+
+public class Box1 {
+    public double w,h,d; //modifier
+    
+    public double volume(){
+        return w*h*d;
+    }
+    public double triangle(){
+        return (0.5)*w*h;
+    }
+}
